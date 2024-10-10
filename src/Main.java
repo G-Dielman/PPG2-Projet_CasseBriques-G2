897cb2c6d201bba28c244500e8a235ffc0d22c3a
2502nosanchezgarcia <2502nosanchezgarcia@student.epfc.eu>
2024-10-10 15:52:07 +0200

    + test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Noah ");
-        System.out.println("Nono");
-        System.out.println("Jim ");
-        System.out.println("test");
+        System.out.println("NEUILLE FRERE");
 
     }
 }
