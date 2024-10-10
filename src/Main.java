8d99c4b309b4081523b2bace2c85f73bc4ab4b53
jim <2604jigerondal@student.epfc.eu>
2024-10-10 15:54:41 +0200

    Msg "Nono"

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Noah ");
         System.out.println("Nono");
+        System.out.println("dsdsdsds");
     }
 }
