b95fe189e0519cf0b5e558ead6bbcd5489f36d5d
2502nosanchezgarcia <2502nosanchezgarcia@student.epfc.eu>
2024-10-10 15:54:55 +0200

    + test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Noah ");
-        int x = 10;
-        System.out.println(x);
+        int y = 10;
+        System.out.println(y);
 
     }
 }
