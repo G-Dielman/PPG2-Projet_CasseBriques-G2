93c306aae5ffc088e37b74a1edef993012987f8a
2502nosanchezgarcia <2502nosanchezgarcia@student.epfc.eu>
2024-10-10 16:05:52 +0200

    test encore

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Noah ");
-        int x = 10;
-        System.out.println(x);
-
-        System.out.println("coucou");
-
-        System.out.println("Nono");
-        System.out.println("dsdsdsds");
-        System.out.println("aaa");
-        System.out.println("banane");
-        System.out.println("écran");
+        System.out.println("FONCTIONNE");
     }
 }
