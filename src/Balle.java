820f1ab5e8a8f82c4dcb84447a17c8ae4423a87b
Noah <2502nosanchezgarcia@student.epfc.eu>
2024-10-14 17:21:50 +0200

    balle

---
+++ src/Balle.java
@@ -0,0 +1,16 @@
+/*Class balle :
+
+Attributs :
+
+position Axe x (double x) et Axe y (double y)
+vitesse vecteur axe x (vx) et vecteur axe y (vy)
+Taille de la balle (Rayon)
+méthodes :
+
+Getter et Setter(modifier la position et modifier la vitesse)
+Méthode "move()" qui bouge la balle avec en ajoutant vx à x et vy à y*/
+
+public class Balle {
+
+
+}
