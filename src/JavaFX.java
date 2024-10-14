0f968b3d9b8781e3911a86231fb558bd5a664daa
2604jigerondal <2604jigerondal@student.epfc.eu>
2024-10-14 19:15:34 +0200

    Correction DIELMAN

--- src/JavaFX.java
+++ src/JavaFX.java
@@ -1,9 +1,23 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class JavaFX extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
     @Override
-    public void start (Stage primaryStage){
+    public void start (Stage stage){
+        HBox hbox = new HBox(10); // 10 pixels d'espacement entre les éléments
+
+        Scene scene = new Scene(hbox, 300, 100);
+        stage.setTitle("HBox Example");
+        stage.setScene(scene);
+        stage.show();
+
 
     }
 }
