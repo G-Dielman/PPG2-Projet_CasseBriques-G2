9e2ba10bbd704497dc5b27d0e9e380008d673a17
NoahLePlusMeilleur <2502nosanchezgarcia@student.epfc.eu>
2024-10-14 17:16:07 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Noah ");
         System.out.println("s'il te plait aller");
+        System.out.println("test ENCORE ENCORE");
     }
 }
