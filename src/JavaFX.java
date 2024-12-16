08a4f191915a6ec234f2d4384d4521883bfe1e0d
saloua <3009samahir@student.epfc.eu>
2024-12-16 20:49:55 +0100

    Class paddel

--- src/JavaFX.java
+++ src/JavaFX.java
@@ -16,62 +16,71 @@ import java.lang.reflect.Array;
 
 public class                                                                        JavaFX extends Application{
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        //création du Pane
+    public void start(Stage primaryStage) {
         Pane root = new Pane();
-        Scene scene = new Scene(root,600,800);
-        primaryStage.setTitle("BrickBreak");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-        //création du rectangle
-
-        Rectangle rec1 = new Rectangle();
-        Paddle pad = new Paddle(25,100,100,750);
-        Rectangle rec = new Rectangle(pad.getPosition().getX(),pad.getPosition().getY(),pad.getTaille().getLargeur(),pad.getTaille().getHauteur());
-        rec.setFill(Color.BLUE);
-        root.getChildren().add(rec);
-
-        //Création de la balle
-
-        //Ball ball1 = new Ball(10,10,5,75,75);
-        //Circle circ1 = new Circle(ball1.getPosition().getX(),ball1.getPosition().getY(),ball1.getRayon());
-       // circ1.setFill(Color.BLACK);
-       // root.getChildren().add(circ1);
-
-        //création des mouvement fait par le joueur
-        scene.setOnKeyPressed(event ->{
+        Scene scene = new Scene(root, 500, 700);
+        // Création de la balle et de la raquette
+        Ball ball = new Ball(scene.getWidth()/2, scene.getHeight()-100, 10, 2, 2);
+        Paddle paddle = new Paddle(scene.getWidth()/2, scene.getHeight()-50 , 100, 20);
+        // Ajouter les formes au pane
+        root.getChildren().addAll(ball.getShape() , paddle.getShape());
+        // Création et affichage des briques. Les briques sont ajoutées au Pane par la fonction et
+        // leurs tailles varie en fonction des dimenssions de la scene
+        Brick[] bricks = createBricks(root, scene);
+        //Déplacement de la raquette
+        scene.setOnKeyPressed(event -> {
             switch (event.getCode()) {
                 case LEFT:
-                    System.out.println();
-                    if (rec.getX()>=0) {
-                        rec.setX(rec.getX() - 30);
-                        pad.setPosition(pad.getPosition().getX() - 30, pad.getPosition().getY());
-                    }
+                    // Empêche de dépasser le bord gauche
+                    paddle.setX(Math.max(0, paddle.getX() - 5));
                     break;
                 case RIGHT:
-                    if (rec.getX()<= 600-pad.getTaille().getLargeur()) {
-                        System.out.println();
-                        rec.setX(rec.getX() + 30);
-                    }
+                    // Empêche de dépasser le bord droit
+                    paddle.setX(Math.min(scene.getWidth() - paddle.getWidth(), paddle.getX() + 5));
                     break;
-
-
-            }});
-
-        //lancement du mouvement de la balle
-
-        AnimationTimer timer = new AnimationTimer() {
+            }
+        });
+        // AnimationTimer pour déplacer la balle
+        this.timer = new AnimationTimer() {
             @Override
-            public void handle(long l) {
-                ball1.move(600,800);
-                circ1.setCenterX(circ1.getCenterX() + ball1.getVitesseX());
-                circ1.setCenterY(circ1.getCenterY() + ball1.getVitesseY());
+            public void handle(long now) {
+                ball.move(); // Met à jour la position de la balle
+                // Rebondir sur les bords de la fenêtre
+                if (ball.getX()- ball.getRadius() <= 0 // Si la balle touche le mur de gauche
+                        || ball.getX() + ball.getRadius() >= scene.getWidth()) // ou le mur de Droite
+                    ball.switchVx(); // Inverser la direction horizontale
+                if (ball.getY() - ball.getRadius() <= 0) // Si la balle touche le plafont
+                    ball.switchVy(); // Inverser la direction verticale
+                // Vérifier si la balle tombe sous la raquette
+                if (ball.getY() > scene.getHeight()) {
+                    stopGame("Game Over!", root);
+                }
+                // Collision avec la raquette
+                if ( paddle.collideWith(ball) ) {
+                    paddle.onCollisionWith(ball);
+                }
+                // Collision avec les briques
+                boolean allBrickBroken = true;
+                for (Brick brick : bricks){
+                    if ( !brick.isBroken() ){
+                        allBrickBroken = false;
+                        if ( brick.collideWith(ball) )
+                            brick.onCollisionWith(ball);
+                    }
+                }
+                //Si toutes les briques sont brisées, le joueur a gagné
+                if(allBrickBroken)
+                    stopGame("YOU WIN !!!", root);
             }
         };
-        timer.start();
+        this.timer.start(); // Démarrer l'animation
 
+        primaryStage.setTitle("Casse-briques - Itération 2");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
+
     public static void main(String[] args){
         launch(args);
     }
--- src/Paddle.java
+++ src/Paddle.java
@@ -1,25 +1,13 @@
-public class Paddle { //tout peut être supprimmé
-    private Position pospaddle = new Position(50,0);
-    private Taille padd = new Taille(50,50);
-    public Paddle(double x, double y) {
-        setPosition(x,y);
-    }
-    public Paddle(double hauteur, double largeur, double x, double y) {
-        setTaille(largeur,hauteur);
-        setPosition(x,y);
-    }
-    public Position getPosition() {
-        return pospaddle;
-    }
-    public Taille getTaille() {return padd;}
-    public void setPosition (double positionx, double positiony) {
-        this.pospaddle.setX(positionx);
-        this.pospaddle.setY(positiony);
-    }
-    public void setTaille (double largeur, double hauteur) {
-        this.padd.setLargeur(largeur);
-        this.padd.setHauteur(hauteur);
-    }
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
 
+public class Paddle extends RectangularGAmeObject {
+
+    private static final Color DEFAULT_COLOR = Color.PURPLE;
+
+    public Paddle(double x, double y, double width, double height) {
+        super( x, y ,width,height, DEFAULT_COLOR);
+
+    }
 
 }
